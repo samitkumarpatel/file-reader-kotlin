3b94fe1a4c2ad177097d6f7cef3c852bc2562b7c
samitkumarpatel <samitkumarpatel@gmail.com>
2024-05-10 00:40:47 +0200

    first commit

---
+++ src/main/kotlin/net/samitkumar/filereaderkotlin/FileReaderKotlinApplication.kt
@@ -0,0 +1,11 @@
+package net.samitkumar.filereaderkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FileReaderKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<FileReaderKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/net/samitkumar/filereaderkotlin/FileReaderKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.samitkumar.filereaderkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FileReaderKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
